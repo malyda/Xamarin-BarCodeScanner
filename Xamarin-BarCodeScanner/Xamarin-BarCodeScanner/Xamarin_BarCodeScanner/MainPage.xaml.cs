82194be7d7ad9ac9d5d8857edd459e07e8a58c1e
David malý <maly.david.it@gmail.com>
2017-03-28 09:17:44 +0200

    init

---
+++ Xamarin-BarCodeScanner/Xamarin-BarCodeScanner/Xamarin_BarCodeScanner/MainPage.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using ZXing.Net.Mobile.Forms;
+
+namespace Xamarin_BarCodeScanner
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+
+            var scanPage = new ZXingScannerPage();
+
+            scanPage.OnScanResult += (result) => {
+                // Stop scanning
+                scanPage.IsScanning = false;
+
+                // Pop the page and show the result
+                Device.BeginInvokeOnMainThread(() => {
+                    Navigation.PopAsync();
+                    DisplayAlert("Scanned Barcode", result.Text, "OK");
+                });
+            };
+
+            // Navigate to our scanner page
+            Navigation.PushAsync(scanPage);
+
+        }
+    }
+}
